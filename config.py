2742e4cbdc7e536715508238cfe155726eb947b0
parvshah010806 <nimish.a116.68@gmail.com>
2021-07-28 19:35:17 +0530

    Update

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ else:
 class Config:
     ADMIN = os.environ.get("ADMINS", '')
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
-    API_ID = int(os.environ.get("API_ID", '7809184'))
+    API_ID = int(float(os.environ.get("API_ID", '7809184')))
     CHAT = int(os.environ.get("CHAT", "-1001591741014"))
     LOG_GROUP=os.environ.get("LOG_GROUP", "")
     if LOG_GROUP:
